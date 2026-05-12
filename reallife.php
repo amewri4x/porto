479c696b70c42c84dad3af8ac78eaa47589b3baa
mewriax <iyasa.miaw@gmail.com>
2026-05-12 18:00:48 +0700

    Add files via upload

---
+++ reallife.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Game Album</title>
+
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<div id="lightbox" onclick="closeImage()">
+
+  <span class="close-btn">&times;</span>
+
+  <img id="lightbox-img">
+
+</div>
+
+<script src="script.js"></script>
+
+<!-- LOADER -->
+<div id="loader">
+  <div class="spinner"></div>
+</div>
+
+<body>
+
+<canvas id="bg-blobs"></canvas>
+
+<nav class="navbar">
+  <div class="logo">SYIFA'</div>
+
+  <ul>
+    <li><a href="index.php">BACK</a></li>
+  </ul>
+</nav>
+
+<section class="page">
+
+    <h1 class="title">Real Life Moment</h1>
+
+
+</section>
+
+</body>
+</html>
