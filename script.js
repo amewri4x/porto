9fd7ba3e6964af6d62f5935e8d886a4c9ffcf939
mewriax <iyasa.miaw@gmail.com>
2026-05-13 08:09:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -117,3 +117,24 @@ window.addEventListener("DOMContentLoaded", () => {
   animate();
 
 });
+
+const hiddenElements =
+document.querySelectorAll(".hidden");
+
+const observer =
+new IntersectionObserver((entries)=>{
+
+  entries.forEach((entry)=>{
+
+    if(entry.isIntersecting){
+
+      entry.target.classList.add("show");
+    }
+
+  });
+
+});
+
+hiddenElements.forEach((el)=>
+observer.observe(el)
+);
